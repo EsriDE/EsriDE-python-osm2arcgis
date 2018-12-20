166f8c01ccc8116218c58dc5c3205ca3acaecbe2
lukasbug <45874502+lukasbug@users.noreply.github.com>
2018-12-20 17:02:02 +0100

    Update osm_runner_utils.py

--- osm_runner_utils.py
+++ osm_runner_utils.py
@@ -1,3 +1,18 @@
+
+__version__ = "0.0.2"
+'''
+__author__ = "Jeffrey Scarmazzi, Lukas Bug"
+__copyright__ = "Copyright 2018, Jeffrey Scarmazzi, Esri Deutschland GmbH"
+__license__ = "Apache-2.0"
+__version__ = "0.0.2"
+__email__ = "s.geigenberger@esri.de, lukas.bug@aol.de"
+The module "osm_runner" was written by Jeffrey Scarmazzi (Jwmazzi) and all copyright belongs to him.
+The original module can be found using the following URL: https://github.com/Jwmazzi/osm_runner
+It was extended to output complex multipolygons based on relation items in OSM.
+This module provides properties to configure the osm_runner module and to control requests to Overpass API.
+Copyright for parts of this version of osm_runner belongs to Jeffrey Scarmazzi.
+'''
+
 # Format: http://wiki.openstreetmap.org/wiki/Overpass_API/Language_Guide
 # Section 13 / 13.1
 Format = '[out:json]'
