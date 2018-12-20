3d6f0540cc64ec58ac80f5775e6ab429af21f585
lukasbug <45874502+lukasbug@users.noreply.github.com>
2018-12-20 21:39:48 +0100

    Update runScript.bat

--- runScript.bat
+++ runScript.bat
@@ -1,1 +1,3 @@
-start "path to python environment"\python.exe "path to python scripts"\MainModule.py
+set path=C:\your_anaconda_install_path\Anaconda3\Library\bin\;%PATH%
+call conda activate OSM2ArcGIS
+call "C:\Users\your_username\AppData\Local\conda\conda\envs\your_python_environment"\python.exe "C:\your_project_extraction_path"\MainModule.py
